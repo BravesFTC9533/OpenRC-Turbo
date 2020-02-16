14faef89f9dc17592296f022de66d099f94a05a8
Nick <manelliproductions@gmail.com>
2020-02-16 12:02:40 -0500

    Made swing move in a little bit more during the position for brick sequence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/LiftController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/LiftController.java
@@ -64,7 +64,7 @@ public class LiftController {
             goTo(1, POSITION_4);
         }
 
-        swing.setPosition(0.8);
+        swing.setPosition(0.85);
 
         ElapsedTime timer = new ElapsedTime();
         timer.reset();
